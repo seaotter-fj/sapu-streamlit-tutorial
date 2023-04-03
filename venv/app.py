4135a9fa91385c7e4efd4634c365c7ee62452dd8
fj_seaotter <fjgenebreak14@gmail.com>
2023-04-03 21:37:26 +0900

    added code captions

--- venv/app.py
+++ venv/app.py
@@ -2,3 +2,15 @@ import streamlit as st
 
 st.title('さぷーアプリ')
 st.caption('This is a test app in the sapu tutorial.')
+
+st.subheader('self-introduction')
+st.text('Pythonに関する情報をYouTubeで発信している、Python VTuber さぷーです。\n'
+        'ぜひ、チャンネル登録してね🎵')
+
+code = '''
+import streamlit as st
+st.title('Sapu app')
+# こんな感じで書いていくよ。
+'''
+
+st.code(code, language='python')
