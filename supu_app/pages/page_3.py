7244a4961b6ff1e16db941ee7188faecba0527d9
fj_seaotter <fjgenebreak14@gmail.com>
2023-04-04 00:39:54 +0900

    fix path maybe

--- supu_app/pages/page_3.py
+++ supu_app/pages/page_3.py
@@ -1,13 +1,14 @@
-import streamlit as st 
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+import streamlit as st 
 
-with open(r'../supu_app/data/data.csv') as csv_file:
+
+path = r'./data/data.csv'
+with open(path) as csv_file:
     df = pd.read_csv(csv_file)
     df.columns = ['one', 'two', 'three']
     
 st.dataframe(df)
-fig, ax = plt.subplots()
-ax.scatter(df['one'], df['two'])
-st.pyplot(fig)
+# fig, ax = plt.subplots()
+# ax.scatter(df['one'], df['two'])
+# st.pyplot(fig)
